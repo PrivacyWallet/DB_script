b723bf9ca6d875b3ff91e86c22edf5b17e60e75f
FKYnJYQ <loveress01@gmail.com>
2020-07-26 20:34:43 +0800

    fix: use query

--- data_query_server.js
+++ data_query_server.js
@@ -1,81 +1,84 @@
-let express = require('express')
-let http = require('http')
-let app = express()
-let dbManager=require('./db_manager')
+let express = require("express");
+let http = require("http");
+let cors = require("cors");
+let dbManager = require("./db_manager");
 
 let config = {
-    PORT: 3000
-}
+  PORT: 3000,
+};
 
-app.use(express.json())
+let app = express();
+app.use(cors());
 
-
+app.use(express.json());
 
 // app.get('/test',async function (req,res) {
 //     return res.status(401).json({error:'query error!'});
 // })
 
-app.get('/getTransactionsAsDataBuyer',async function (req,res) {
-    let address=req.body.address
-    let results=await dbManager.getTransactionsAsDataBuyer(address,function(results)
-    {
-        res.send(results);
+app.get("/getTransactionsAsDataBuyer", async function (req, res) {
+  let address = req.query.address;
+  let results = await dbManager.getTransactionsAsDataBuyer(
+    address,
+    function (results) {
+      res.send(results);
     },
-    function onfail()
-    {
-        console.error('query error!')
-        return res.status(401).json({error:'query error!'});
-    })
-})
+    function onfail() {
+      console.error("query error!");
+      return res.status(401).json({ error: "query error!" });
+    }
+  );
+});
 
-
-app.get('/getTransactionsAsDataOwner',async function (req,res) {
-    //这里需要确定查dataowner的时候传入的究竟是address还是dataBuyerContractAddress
-    let address=req.body.address
-    let results=await dbManager.getTransactionsAsDataOwner(address,function(results)
-    {
-        res.send(results);
+app.get("/getTransactionsAsDataOwner", async function (req, res) {
+  //这里需要确定查dataowner的时候传入的究竟是address还是dataBuyerContractAddress
+  console.log(req.query);
+  let address = req.query.address;
+  let results = await dbManager.getTransactionsAsDataOwner(
+    address,
+    function (results) {
+      res.send(results);
     },
-    function onfail()
-    {
-        console.error('query error!')
-        return res.status(401).json({error:'query error!'});
-    })
-})
+    function onfail() {
+      console.error("query error!");
+      return res.status(401).json({ error: "query error!" });
+    }
+  );
+});
 
-app.get('/getCalculators',async function (req,res) {
-    let results=await dbManager.getCalculators(function(results)
-    {
-        res.send(results);
+app.get("/getCalculators", async function (req, res) {
+  let results = await dbManager.getCalculators(
+    function (results) {
+      res.send(results);
     },
-    function onfail()
-    {
-        console.error('query error!')
-        return res.status(401).json({error:'query error!'});
-    })
-})
+    function onfail() {
+      console.error("query error!");
+      return res.status(401).json({ error: "query error!" });
+    }
+  );
+});
 
-app.post('/addCalculator',async function (req,res) {
-    let calculator=req.body.calculator
-    await dbManager.addCalculator(address)
-})
+app.post("/addCalculator", async function (req, res) {
+  let calculator = req.body.calculator;
+  await dbManager.addCalculator(address);
+});
 
-app.get('/getData',async function (req,res) {
-    let address=getAddress(req)
-    let results=await dbManager.getTransactionsAsDataBuyer(address,function(results)
-    {
-        res.send(results);
+app.get("/getData", async function (req, res) {
+  let address = getAddress(req);
+  let results = await dbManager.getTransactionsAsDataBuyer(
+    address,
+    function (results) {
+      res.send(results);
     },
-    function onfail()
-    {
-        console.error('query error!')
-        return res.status(401).json({error:'query error!'});
-    })
-})
+    function onfail() {
+      console.error("query error!");
+      return res.status(401).json({ error: "query error!" });
+    }
+  );
+});
 
-
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
-})
+  console.log(`Server running on port ${config.PORT}`);
+});
